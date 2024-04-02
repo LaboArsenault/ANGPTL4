a7fa77563d7a849e031b79ec2c318714b3164dc3
gagelo01 <eloi.gagnon.1@ulaval.ca>
2024-04-02 11:17:45 -0400

    perfrom the E40K analysis scaled on TG levels

--- Analysis/1b_resper_tgsd2.R
+++ Analysis/1b_resper_tgsd2.R
@@ -1,18 +1,15 @@
 #!/usr/bin/env Rscript
 setwd("/home/gagelo01/workspace/Projects/small_MR_exploration/Test_potential_project/ANGPTL4")
 source("Analysis/tosource.R")
-source("/mnt/sda/gagelo01/Projects/small_MR_exploration/F2_finngen/Analysis/func_F2.R")
 
 out <- fread("Data/Modified/all_outcome.txt" )
 
-inst <- out[id.outcome=="trait-16-4", ]
+inst <- out[id.outcome=="trait-16-4" & SNP=="rs116843064", ]
 colnom<-colnames(inst)[grepl("outcome", colnames(inst))]
 setnames(inst, colnom, gsub("outcome", "exposure", colnom))
-# harm <- TwoSampleMR::harmonise_data(inst[SNP=="rs116843064",],
-#                             outcome_dat = out[!grepl("dis-15", id.outcome),],
-#                             action = 1)
 
-res <- wald_quick(inst_all_sign_clump_arg = inst[SNP=="rs116843064",], all_outcome_arg = out[!grepl("dis-15", id.outcome),], 
+
+res <- wald_quick(inst_all_sign_clump_arg = inst, all_outcome_arg = unique(out[!grepl("dis-15", id.outcome),]), 
                   id_exposure_name = "trait-16-4")
 
 fwrite(res, "Data/Modified/res_tg_sd.txt")
