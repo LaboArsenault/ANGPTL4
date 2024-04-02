23e39afa6e72512c7deb9cf0cadcdb4ed4a9ceed
gagelo01 <eloi.gagnon.1@ulaval.ca>
2024-04-02 11:22:48 -0400

    write the drug_pipeline.R

--- Analysis/write_drugpipeline.R
+++ Analysis/write_drugpipeline.R
@@ -1,21 +1,9 @@
 #!/usr/bin/env Rscript
-#####Section to CHange ##########
 setwd("/mnt/sda/gagelo01/Projects/small_MR_exploration/Test_potential_project/ANGPTL4")
 mysource <- "tosource.R"
 
-myArray=list.files("/mnt/sda/gagelo01/Projects/Pipelines/Robust_drug_pipeline/Analysis/")
-myArray=myArray[grepl("^[1-9]", myArray) & grepl("\\.R$", myArray)]
-myArray <- myArray[c(2:5, 9)]
-myArray <- paste0("/mnt/sda/gagelo01/Projects/Pipelines/Robust_drug_pipeline/Analysis/", myArray)
-##################################
-bloc1<- paste0("myArray=(", paste(myArray, collapse = " "), ")")
-
-bloc2 <- "for str in ${myArray[@]}; do
-chmod u+x $str
-done"
-
-bloc3 <- paste(paste(paste0("echo 'Initializing ", myArray, "' && ", myArray, " ", mysource," &&"), collapse = " "), "echo 'The master script finished without errors'")
-
-fileConn<-file("Analysis/drugpipeline.sh")
-writeLines(paste(c("#!/bin/bash",bloc1, bloc2, bloc3), collapse = "\n"), fileConn)
-close(fileConn)
+library(GagnonMR)
+myArray <- GagnonMR::list_masterscript("/mnt/sda/gagelo01/Projects/Pipelines/Robust_efficient_drug_pipeline/Analysis/")
+myArray <- myArray[c(2:5, 8)]
+GagnonMR::write_masterscript(myArray = myArray, mysource = mysource, output_name = "Analysis/drugpipeline.sh")
+message("This script finished without errors")
